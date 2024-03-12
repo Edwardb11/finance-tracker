9ab964b6f7d5f2cf78b29c29ce3b6af018dda9da
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-12 09:08:15 -0400

    feat: context for auth

---
+++ lib/store/auth-context.tsx
@@ -0,0 +1,67 @@
+import { createContext, ReactNode } from "react";
+import { auth } from "@/lib/firebase";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+type User = {
+  uid: string;
+  email: string;
+  displayName: string | null;
+  photoURL: string | null;
+};
+
+type AuthContextType = {
+  user: User | null;
+  loading: boolean;
+  googleLoginHandler: () => Promise<void>;
+  logout: () => void;
+};
+
+export const authContext = createContext<AuthContextType>({
+  user: null,
+  loading: false,
+  googleLoginHandler: async () => {},
+  logout: () => {},
+});
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function AuthContextProvider({
+  children,
+}: AuthContextProviderProps) {
+  const [user, loading] = useAuthState(auth);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const googleLoginHandler = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const logout = () => {
+    signOut(auth);
+  };
+
+  const currentUser: User | null = user
+    ? {
+        uid: user.uid,
+        email: user.email || "",
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+      }
+    : null;
+
+  const values: AuthContextType = {
+    user: currentUser,
+    loading,
+    googleLoginHandler,
+    logout,
+  };
+
+  return <authContext.Provider value={values}>{children}</authContext.Provider>;
+}
