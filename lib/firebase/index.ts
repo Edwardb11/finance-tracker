90201e712ead28d4037a890df7f32b25224c878a
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-13 12:34:26 -0400

    feat: env

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -7,13 +7,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmHueq__Wt_74N1VJRxzsj5xmDzbKAbhg",
-  authDomain: "finance-trancker.firebaseapp.com",
-  projectId: "finance-trancker",
-  storageBucket: "finance-trancker.appspot.com",
-  messagingSenderId: "376752538568",
-  appId: "1:376752538568:web:228c2e379ca482e2e9b6fd",
-};
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+}
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
